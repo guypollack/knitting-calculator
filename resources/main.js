c8092155112454a749aac0f1eecc31cf9cac404d
guypollack <84754057+guypollack@users.noreply.github.com>
2022-05-25 08:17:05 +0100

    Add files via upload

--- resources/main.js
+++ resources/main.js
@@ -18,6 +18,117 @@ function stitches(hLength, vLength, direction, totalRows, swatchStitches, swatch
     return [stitchesArray,rowsInSection];
 }
 
+function findNumberOfExistingRows() {
+    const leftFormInputs = document.getElementById("left-form-inputs");
+    return leftFormInputs.getElementsByTagName("input").length / 4;
+}
+
+function createInputElement(type,id,_name,_class) {
+    const element = document.createElement("input");
+    element.setAttribute("type",type);
+    element.setAttribute("id",id);
+    element.setAttribute("name",_name);
+    element.setAttribute("class",_class);
+    //element.setAttribute("value",value);
+    //element.setAttribute("style","margin-left:10px");
+    return element
+}
+
+function addStyling(element) {
+    let _class = element.classList;
+    _class = String(_class);
+
+    switch (_class) {
+        case "col-left":
+            element.setAttribute("style","margin-left:2px");
+            break;
+        case "col-mid":
+            element.setAttribute("style","margin-left:9px");
+            break;
+        case "i-col-right":
+            element.setAttribute("style","margin-left:15px");
+            break;
+        case "d-col-right":
+            element.setAttribute("style","margin-left:14.5px");
+            break;
+        default:
+            window.alert("Style not applied");
+            break;
+    }
+}
+
+function addInputRow() {
+    const newRowNumber = findNumberOfExistingRows() + 1;
+    const leftFormDiv = document.getElementById("left-form-inputs");
+    const rightFormDiv = document.getElementById("right-form-inputs");
+    
+    let newLeftInputRowLabelText = ""
+    if (newRowNumber < 10) {
+        newLeftInputRowLabelText = `${newRowNumber}. `
+    } else {
+        newLeftInputRowLabelText = `${newRowNumber}.`
+    }
+    let newRightInputRowLabelText = ""
+    if (newRowNumber < 10) {
+        newRightInputRowLabelText = `${newRowNumber}. `
+    } else {
+        newRightInputRowLabelText = `${newRowNumber}.`
+    }
+
+    const newLeftInputRowContainer = document.createElement("div");
+    const newLeftInputRowLabelNode = document.createElement("label");
+    const newLeftInputRowLabel = document.createTextNode(`${newLeftInputRowLabelText}`);
+    const newRightInputRowContainer = document.createElement("div");
+    const newRightInputRowLabelNode = document.createElement("label");
+    const newRightInputRowLabel = document.createTextNode(`${newRightInputRowLabelText}`);
+
+    const leftHorInputBox = createInputElement("text",`l-hchange-${newRowNumber}`,`l-hchange-${newRowNumber}`,"col-left");
+    addStyling(leftHorInputBox);
+    const leftVertInputBox = createInputElement("text",`l-vchange-${newRowNumber}`,`l-vchange-${newRowNumber}`,"col-mid");
+    addStyling(leftVertInputBox);
+    const leftIRadioButton = createInputElement("radio",`l-i-${newRowNumber}`,`l-i-d-${newRowNumber}`,"i-col-right");
+    addStyling(leftIRadioButton);
+    const leftDRadioButton = createInputElement("radio",`l-d-${newRowNumber}`,`l-i-d-${newRowNumber}`,"d-col-right");
+    addStyling(leftDRadioButton);
+
+    const rightHorInputBox = createInputElement("text",`r-hchange-${newRowNumber}`,`r-hchange-${newRowNumber}`,"col-left");
+    addStyling(rightHorInputBox);
+    const rightVertInputBox = createInputElement("text",`r-vchange-${newRowNumber}`,`r-vchange-${newRowNumber}`,"col-mid");
+    addStyling(rightVertInputBox);
+    const rightIRadioButton = createInputElement("radio",`r-i-${newRowNumber}`,`r-i-d-${newRowNumber}`,"i-col-right");
+    addStyling(rightIRadioButton);
+    const rightDRadioButton = createInputElement("radio",`r-d-${newRowNumber}`,`r-i-d-${newRowNumber}`,"d-col-right");
+    addStyling(rightDRadioButton);
+
+    
+    newLeftInputRowContainer.appendChild(newLeftInputRowLabelNode);
+    newLeftInputRowLabelNode.appendChild(newLeftInputRowLabel);
+    newLeftInputRowContainer.appendChild(leftHorInputBox);
+    newLeftInputRowContainer.appendChild(leftVertInputBox);
+    newLeftInputRowContainer.appendChild(leftIRadioButton);
+    newLeftInputRowContainer.appendChild(leftDRadioButton);
+    newLeftInputRowContainer.appendChild(document.createElement("br"));
+    leftFormDiv.appendChild(newLeftInputRowContainer)
+
+    newRightInputRowContainer.appendChild(newRightInputRowLabelNode);
+    newRightInputRowLabelNode.appendChild(newRightInputRowLabel);
+    newRightInputRowContainer.appendChild(rightHorInputBox);
+    newRightInputRowContainer.appendChild(rightVertInputBox);
+    newRightInputRowContainer.appendChild(rightIRadioButton);
+    newRightInputRowContainer.appendChild(rightDRadioButton);
+    newRightInputRowContainer.appendChild(document.createElement("br"));
+    rightFormDiv.appendChild(newRightInputRowContainer)
+}
+
+function removeInputRow() {
+    const lastRowNumber = findNumberOfExistingRows();
+
+    if (lastRowNumber > 1) {
+        document.getElementById(`l-hchange-${lastRowNumber}`).parentNode.remove();
+        document.getElementById(`r-hchange-${lastRowNumber}`).parentNode.remove();
+    }
+}
+
 function findDirection(side,number) {
     if (document.getElementById(id=`${side}-d-${number}`).checked) {
         return "d";
@@ -41,8 +152,10 @@ function calculateInstructions() {
     const castOn = Math.round(+document.getElementById("cast-on").value * swatchStitches) + 1;
     let castOff = castOn;
 
-    const inputsL = extractInputs("l",1,8);
-    const inputsR = extractInputs("r",1,8);
+    const inputRows = findNumberOfExistingRows();
+
+    const inputsL = extractInputs("l",1,inputRows);
+    const inputsR = extractInputs("r",1,inputRows);
 
     let addedStitchesL = [];
     let totalRowsL = 0;
@@ -87,7 +200,7 @@ function calculateInstructions() {
             //castOff += Number(stitchTotals[elem][0]);
         }
         else {
-            if (elem in stitchTotals) {
+            if (elem * -1 in stitchTotals) {
                 stitchTotals[elem * -1][0] = addedStitchesL.filter(value => value === elem).length * -1;
             } else {
                 stitchTotals[elem * -1] = [addedStitchesL.filter(value => value === elem).length * -1,"  "];
@@ -102,6 +215,7 @@ function calculateInstructions() {
 
     if (tableExists) {
         document.getElementById("cast-on-text").remove()
+        document.getElementById("line-break").remove()
         document.getElementById("cast-off-text").remove()
         document.getElementById("instructions-table").remove()
     }
@@ -136,6 +250,7 @@ function calculateInstructions() {
 
         let newCell3 = newRow.insertCell();
         let instructionsR = `${stitchTotals[elem][1]}`;
+        
         let newText3 = document.createTextNode(instructionsR);
         if (instructionsR[0] === "+") {
             newCell3.setAttribute("style","color:green");
@@ -152,14 +267,22 @@ function calculateInstructions() {
     castOnNode.setAttribute("id","cast-on-text");
     castOnNode.appendChild(document.createTextNode(""));
     
+    const lineBreak = document.createElement("br");
+    lineBreak.setAttribute("id","line-break");
+
     const castOffNode = document.createElement("p");
     castOffNode.setAttribute("class","cast-instruction");
     castOffNode.setAttribute("id","cast-off-text");
     castOffNode.appendChild(document.createTextNode(""));
+    
     const parentDiv = document.getElementById("instructions-table").parentNode;
     const tableElement = document.getElementById("instructions-table");
+    
     parentDiv.insertBefore(castOnNode,tableElement);
     document.getElementById("cast-on-text").innerHTML = `Cast On: <span>${castOn}</span>`;
+    
+    parentDiv.insertBefore(lineBreak,tableElement);
+    
     parentDiv.insertBefore(castOffNode,tableElement);
     document.getElementById("cast-off-text").innerHTML = `Cast Off: <span>${castOff}</span>`;
 }
