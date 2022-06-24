207b5bb7823a76b150de4004547bac9f842ac46a
guypollack <84754057+guypollack@users.noreply.github.com>
2022-06-24 15:17:02 +0100

    Add functions, simplify pasting to results area
    Add functions for loading and saving patterns
    Incorporate new inputs from top into results area
    Incorporate adding Details to results table
    Simplify pasting to results area by using fewer text nodes and putting \n characters into the nodes, rather than separate nodes

--- resources/main.js
+++ resources/main.js
@@ -18,9 +18,9 @@ function stitches(hLength, vLength, direction, totalRows, swatchStitches, swatch
     return [stitchesArray,rowsInSection];
 }
 
-function findNumberOfExistingRows() {
-    const leftFormInputs = document.getElementById("left-form-inputs");
-    return leftFormInputs.getElementsByTagName("input").length / 4;
+function findNumberOfExistingRows(id,divisor) {
+    const div = document.getElementById(id);
+    return div.getElementsByTagName("input").length / divisor;
 }
 
 function createInputElement(type,id,_name,_class) {
@@ -56,7 +56,7 @@ function addStyling(element) {
 }
 
 function addInputRow() {
-    const newRowNumber = findNumberOfExistingRows() + 1;
+    const newRowNumber = findNumberOfExistingRows("left-form-inputs",4) + 1;
     const leftFormDiv = document.getElementById("left-form-inputs");
     const rightFormDiv = document.getElementById("right-form-inputs");
     
@@ -118,7 +118,7 @@ function addInputRow() {
 }
 
 function removeInputRow() {
-    const lastRowNumber = findNumberOfExistingRows();
+    const lastRowNumber = findNumberOfExistingRows("left-form-inputs",4);
 
     if (lastRowNumber > 1) {
         document.getElementById(`l-hchange-${lastRowNumber}`).parentNode.remove();
@@ -126,8 +126,49 @@ function removeInputRow() {
     }
 }
 
+function addDetailRow() {
+    const newRowNumber = findNumberOfExistingRows("details-inputs",3) + 1;
+    const detailsInputArea = document.getElementById("details-inputs");
+
+    let newDetailInputRowLabelText = ""
+    if (newRowNumber < 10) {
+        newDetailInputRowLabelText = `${newRowNumber}. `
+    } else {
+        newDetailInputRowLabelText = `${newRowNumber}.`
+    }
+
+    const newDetailInputRowContainer = document.createElement("div");
+    const newDetailInputRowLabelNode = document.createElement("label");
+    const newDetailInputRowLabel = document.createTextNode(newDetailInputRowLabelText);
+    
+    const detailInputBox = createInputElement("text",`detail-${newRowNumber}`,`detail-${newRowNumber}`,"col-left");
+    detailInputBox.setAttribute("style","margin-left:-3px");
+    const detailAboveInputBox = createInputElement("text",`detail-${newRowNumber}-above`,`detail-${newRowNumber}-above`,"col-mid");
+    detailAboveInputBox.setAttribute("style","margin-left:14.5px");
+    const detailHeightInputBox = createInputElement("text",`detail-${newRowNumber}-height`,`detail-${newRowNumber}-height`,"col-right");
+    detailHeightInputBox.setAttribute("style","margin-left:19.5px");
+    
+    
+    newDetailInputRowContainer.appendChild(newDetailInputRowLabelNode);
+    newDetailInputRowLabelNode.appendChild(newDetailInputRowLabel);
+    newDetailInputRowContainer.appendChild(detailInputBox);
+    newDetailInputRowContainer.appendChild(detailAboveInputBox);
+    newDetailInputRowContainer.appendChild(detailHeightInputBox);
+    //newDetailInputRowContainer.appendChild(document.createElement("br")); Not needed
+    detailsInputArea.appendChild(newDetailInputRowContainer)
+}
+
+function removeDetailRow() {
+    const lastRowNumber = findNumberOfExistingRows("details-inputs",3);
+
+    if (lastRowNumber > 1) {
+        document.getElementById(`detail-${lastRowNumber}-height`).parentNode.remove();
+        document.getElementById(`detail-${lastRowNumber}`).parentNode.remove();
+    }
+}
+
 function findDirection(side,number) {
-    if (document.getElementById(id=`${side}-d-${number}`).checked) {
+    if (document.getElementById(`${side}-d-${number}`).checked) {
         return "d";
     } else {
         return "i";
@@ -137,22 +178,58 @@ function findDirection(side,number) {
 function extractInputs(side,start,end) {
     const inputs = {};
     for (let i = start; i <= end; i++) {
-        inputs[i] = [+document.getElementById(id=`${side}-hchange-${i}`).value,+document.getElementById(id=`${side}-vchange-${i}`).value,findDirection(side,i)];
+        inputs[i] = [+document.getElementById(`${side}-hchange-${i}`).value,+document.getElementById(`${side}-vchange-${i}`).value,findDirection(side,i)];
     }
     return inputs;
 }
 
+function extractDetails() {
+    const details = [];
+    for (let i = 1; i <= findNumberOfExistingRows("details-inputs",3); i++) {
+        details.push([document.getElementById(`detail-${i}`).value,+document.getElementById(`detail-${i}-above`).value,+document.getElementById(`detail-${i}-height`).value]);
+    }
+    return details;
+}
+
+function calculateRowsUpToSectionInclusive(section,swatchRows) {
+    
+    if (section > findNumberOfExistingRows("left-form-inputs",4)) return;
+    
+    let rows = 0;
+
+    for (let i = 1; i <= section; i++) {
+        rows += Math.round(+document.getElementById(`l-vchange-${i}`).value * swatchRows); //NB did not just count full vertical length up to & including this row then multiply, because each section is calculated separately for stitch adding elsewhere, and so results between here and elsewhere may not match due to rounding
+    }
+    return rows;
+}
+
+function calculateRibRows(ribLength,swatchRows) {
+    return Math.round(ribLength * swatchRows);
+}
+
 function calculateInstructions() {
 
-    const swatchStitches = +document.getElementById(id="swatch-stitches").value;
-    const swatchRows = +document.getElementById(id="swatch-rows").value;
+    const swatchStitches = +document.getElementById("swatch-stitches").value;
+    const swatchRows = +document.getElementById("swatch-rows").value;
     const castOn = Math.round(+document.getElementById("cast-on").value * swatchStitches) + 1;
+
     let castOff = castOn;
 
-    const inputRows = findNumberOfExistingRows();
+    let castMultiplier = 1;
+
+    if (document.getElementById("times-2").checked) {
+        castMultiplier = 2;
+    }
+
+    const ribLength = +document.getElementById("rib").value;
+
+    const ribRows = calculateRibRows(ribLength,swatchRows);
+
+    const inputRows = findNumberOfExistingRows("left-form-inputs",4);
 
     const inputsL = extractInputs("l",1,inputRows);
     const inputsR = extractInputs("r",1,inputRows);
+    const detailsInputs = extractDetails();
 
     let addedStitchesL = [];
     let totalRowsL = 0;
@@ -172,14 +249,20 @@ function calculateInstructions() {
         totalRowsR += stitchesResultsR[1];
     } 
 
+    /*let rowDetails = {
+        2: "add button",
+        4: "add button",
+        15: "detail"
+    }*/
+
     let stitchTotals = {};
 
     for (elem of addedStitchesR) {
         if (elem > 0) {
-            stitchTotals[elem] = ["  ",`+${addedStitchesR.filter(value => value === elem).length}`];
+            stitchTotals[elem] = ["  ",`+${addedStitchesR.filter(value => value === elem).length}`,"  "];
         }
         else {
-            stitchTotals[elem * -1] = ["  ",addedStitchesR.filter(value => value === elem).length * -1];
+            stitchTotals[elem * -1] = ["  ",addedStitchesR.filter(value => value === elem).length * -1,"  "];
         }
         
     }
@@ -189,39 +272,86 @@ function calculateInstructions() {
             if (elem in stitchTotals) {
                 stitchTotals[elem][0] = `+${addedStitchesL.filter(value => value === elem).length}`;
             } else {
-                stitchTotals[elem] = [`+${addedStitchesL.filter(value => value === elem).length}`,"  "];
+                stitchTotals[elem] = [`+${addedStitchesL.filter(value => value === elem).length}`,"  ","  "];
             }
         }
         else {
             if (elem * -1 in stitchTotals) {
                 stitchTotals[elem * -1][0] = addedStitchesL.filter(value => value === elem).length * -1;
             } else {
-                stitchTotals[elem * -1] = [addedStitchesL.filter(value => value === elem).length * -1,"  "];
+                stitchTotals[elem * -1] = [addedStitchesL.filter(value => value === elem).length * -1,"  ","  "];
+            }
+        }
+    }
+
+    /*for (elem of Object.keys(rowDetails)) {
+        if (elem in stitchTotals) {
+            stitchTotals[elem][2] = rowDetails[elem];
+        } else {
+            stitchTotals = ["  ","  ",rowDetails[elem]];
+        }
+    }*/
+
+    for (i = 0; i < detailsInputs.length; i++) {
+        const detailRow = Math.round(calculateRowsUpToSectionInclusive(detailsInputs[i][1],swatchRows)) + Math.round(detailsInputs[i][2] * swatchRows);
+        if (detailRow > 0) {
+            if (detailRow in stitchTotals) {
+                console.log(stitchTotals[detailRow][2] === "  ")
+                if (stitchTotals[detailRow][2] === "  ") {
+                    stitchTotals[detailRow][2] = detailsInputs[i][0];
+                } else {
+                    stitchTotals[detailRow][2] = `${stitchTotals[detailRow][2]}; ${detailsInputs[i][0]}`;
+                }
+            } else {
+                stitchTotals[detailRow] = ["  ","  ",detailsInputs[i][0]];
             }
         }
     }
 
     let resultsArea = document.getElementById("results-area");
 
-    let tableExists = !!document.getElementById("instructions-table");
+    let instructionsExist = !!document.getElementById("small-instruction-text");
 
-    if (tableExists) {
-        document.getElementById("cast-on-text").remove()
+    if (instructionsExist) {
+        document.getElementById("small-instruction-text").remove()
         document.getElementById("line-break").remove()
-        document.getElementById("cast-off-text").remove()
+        document.getElementById("cast-on-off-text").remove()
         document.getElementById("instructions-table").remove()
     }
 
-    const castOnNode = document.createElement("p");
-    castOnNode.setAttribute("class","cast-instruction");
-    castOnNode.setAttribute("id","cast-on-text");
-    castOnNode.appendChild(document.createTextNode(""));
-    resultsArea.appendChild(castOnNode);
-    document.getElementById("cast-on-text").innerHTML = `Cast On: <span>${castOn}</span>`;
+    if (totalRowsR + totalRowsL === 0) {
+        return;
+    }
+
+    const smallInstructionsNode = document.createElement("p");
+    smallInstructionsNode.setAttribute("class","small-instruction");
+    smallInstructionsNode.setAttribute("id","small-instruction-text");
+    smallInstructionsNode.setAttribute("style","text-align:left")
+    const smallInstructionText = `Name: ${document.getElementById("name").value} \n \
+                                    Yarn: ${document.getElementById("yarn").value} \n \
+                                    Tension: ${document.getElementById("tension").value} \n \
+                                    Ends: ${document.getElementById("ends").value} \n \
+                                    Rib: ${ribRows} rows \n \
+                                    Notes: ${document.getElementById("notes").value}`
+    smallInstructionsNode.appendChild(document.createTextNode(smallInstructionText));
+    resultsArea.appendChild(smallInstructionsNode);
 
     const lineBreak = document.createElement("br");
     lineBreak.setAttribute("id","line-break");
     resultsArea.appendChild(lineBreak);
+
+    for (elem of Object.keys(stitchTotals).sort((a,b) => b-a)) {
+        castOff += Number(stitchTotals[elem][0]) + Number(stitchTotals[elem][1]);
+    }
+
+    const castOnOffNode = document.createElement("p");
+    castOnOffNode.setAttribute("class","cast-instruction");
+    castOnOffNode.setAttribute("id","cast-on-off-text");
+    castOnOffNode.setAttribute("style","margin-top:16pt");
+    castOnOffNode.appendChild(document.createTextNode(""));
+    resultsArea.appendChild(castOnOffNode);
+    document.getElementById("cast-on-off-text").innerHTML = `Cast On: <span>${castOn} x ${castMultiplier}</span> \n \n \
+                                                                Cast Off: <span>${castOff} x ${castMultiplier}</span>`;
 
     let table = document.createElement("table");
     table.setAttribute("id","instructions-table");
@@ -231,11 +361,12 @@ function calculateInstructions() {
 
     for (elem of Object.keys(stitchTotals).sort((a,b) => b-a)) {
 
-        castOff += Number(stitchTotals[elem][0]) + Number(stitchTotals[elem][1]);
-
         let newRow = tableBody.insertRow();
-        let newCell1 = newRow.insertCell();
+        
+        let newCell0 = newRow.insertCell();
+        newCell0.setAttribute("style","border-top:none ; border-bottom:none ; border-left:none")
 
+        let newCell1 = newRow.insertCell();
         let instructionsL = `${stitchTotals[elem][0]} \t`;
         let newText1 = document.createTextNode(instructionsL);
         if (instructionsL[0] === "+") {
@@ -252,7 +383,6 @@ function calculateInstructions() {
 
         let newCell3 = newRow.insertCell();
         let instructionsR = `${stitchTotals[elem][1]}`;
-        
         let newText3 = document.createTextNode(instructionsR);
         if (instructionsR[0] === "+") {
             newCell3.setAttribute("style","color:green");
@@ -261,12 +391,174 @@ function calculateInstructions() {
             newCell3.setAttribute("style","color:orange");
         }
         newCell3.appendChild(newText3);
+
+        let newCell4 = newRow.insertCell();
+        newCell4.setAttribute("style","font-size:8pt ; border-top:none ; border-bottom:none ; border-right:none")
+        let details = stitchTotals[elem][2];
+        let newText4 = document.createTextNode(details);
+        newCell4.appendChild(newText4);
     }
-    
-    const castOffNode = document.createElement("p");
-    castOffNode.setAttribute("class","cast-instruction");
-    castOffNode.setAttribute("id","cast-off-text");
-    castOffNode.appendChild(document.createTextNode(""));
-    resultsArea.insertBefore(castOffNode,table);
-    document.getElementById("cast-off-text").innerHTML = `Cast Off: <span>${castOff}</span>`;
 }
+
+function listAllInputElements() {
+    const allElements = ["swatch-stitches","swatch-rows","name","notes","cast-on","times-2","yarn","tension","ends","rib"];
+    for (i = 1; i <= findNumberOfExistingRows("left-form-inputs",4); i++) {
+        allElements.push(`l-hchange-${i}`,`l-vchange-${i}`,`l-i-${i}`,`l-d-${i}`,`r-hchange-${i}`,`r-vchange-${i}`,`r-i-${i}`,`r-d-${i}`);
+    }
+    for (i = 1; i <= findNumberOfExistingRows("details-inputs",3); i++) {
+        //window.alert(`detail-${i}`,`detail-${i}-above`,`detail-${i}-height`);
+        allElements.push(`detail-${i}`,`detail-${i}-above`,`detail-${i}-height`);
+    }
+    //window.alert(allElements);
+    return allElements;
+}
+
+function writeTextForFile() {
+    
+    const allElements = listAllInputElements();
+    let text = "";
+    let val = "";
+    
+    for (elem of allElements) {   
+        console.log(elem);         
+        if (elem.slice(1,4) === "-i-" || elem.slice(1,4) === "-d-" || elem === "times-2") {
+            val = document.getElementById(elem).checked;
+        } else {
+            val = document.getElementById(elem).value;
+        }
+        console.log(val);
+        text += `${elem},${val};`;
+    }
+
+    text = text.slice(0,text.length-1);
+
+    return text;
+
+}
+
+function createTextFile() {
+ 
+    let fileNameToSaveAs = prompt("Enter file name to save pattern as, or leave blank to use Name")
+
+
+    if (fileNameToSaveAs != null) {
+
+        if (fileNameToSaveAs === "") {
+            fileNameToSaveAs = document.getElementById("name").value;
+        }
+
+        const textToWrite = writeTextForFile();
+
+        //window.alert(textToWrite);
+
+        const textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+        
+        const downloadLink = document.createElement("a");
+        
+        downloadLink.download = fileNameToSaveAs;
+        downloadLink.innerHTML = "Download File";
+        if (window.webkitURL != null)
+        {
+            // Chrome allows the link to be clicked
+            // without actually adding it to the DOM.
+            downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+        }
+        else
+        {
+            // Firefox requires the link to be added to the DOM
+            // before it can be clicked.
+            downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+            downloadLink.onclick = destroyClickedElement;
+            downloadLink.style.display = "none";
+            document.body.appendChild(downloadLink);
+        }
+        downloadLink.click();
+    }
+}
+
+function addOrRemoveSections(text) {
+    const values = text.split(";");
+    
+    let sectionsInText = 0;
+    let detailRowsInText = 0;
+    
+    for (let i = 0; i < values.length; i++) {
+        if (values[i].slice(1,4) === "-i-" || values[i].slice(1,4) === "-d-") {
+            sectionsInText++;
+        } else if (values[i].slice(0,7) === "detail-") {
+            detailRowsInText++
+        }
+    }
+
+    sectionsInText /= 4;
+    detailRowsInText /= 3;
+    
+    //const sectionsInText = (values.length - 7) / 8; //7 fields at top, 2 sides, 4 inputs in each row on each side
+    
+    const currentSections = findNumberOfExistingRows("left-form-inputs",4);
+    const currentDetailRows = findNumberOfExistingRows("details-inputs",3);
+
+    //console.log(sectionsInText,currentSections);
+
+    if (sectionsInText != currentSections) {
+        const sectionDifference = sectionsInText - currentSections;
+        if (sectionDifference > 0) {
+            for (let i = 1; i <= sectionDifference; i++) {
+                addInputRow();
+            }
+        } else {
+            for (let i = 1; i <= Math.abs(sectionDifference); i++) {
+                removeInputRow();
+            }
+        }
+    }
+
+    if (detailRowsInText != currentDetailRows) {
+        const detailRowsDifference = detailRowsInText - currentDetailRows;
+        if (detailRowsDifference > 0) {
+            for (let i = 1; i <= detailRowsDifference; i++) {
+                addDetailRow();
+            }
+        } else {
+            for (let i = 1; i <= Math.abs(detailRowsDifference); i++) {
+                removeDetailRow();
+            }
+        }
+    }
+}
+
+function populatePage(text) {
+    const values = text.split(";");
+    for (i = 0; i < values.length; i++) {
+        let elementValueArray = values[i].split(",");
+    
+        if (elementValueArray[0].slice(1,4) === "-i-" || elementValueArray[0].slice(1,4) === "-d-" || elementValueArray[0] === "times-2") {
+            document.getElementById(elementValueArray[0]).checked = elementValueArray[1] === "true";
+        } else {
+            document.getElementById(elementValueArray[0]).value = elementValueArray[1];
+        }
+    }
+    document.getElementById("submit-button").click();
+}
+
+let input = document.getElementById("file-picker");
+
+input.addEventListener("change", () => {
+    let files = input.files;
+
+    if (files.length === 0) return;
+
+    const file = files[0];
+
+    let reader = new FileReader();
+
+    reader.onload = (e) => {
+        const file = e.target.result;
+        addOrRemoveSections(file);
+        populatePage(file);
+    }
+
+    reader.onerror = (e) => alert(e.target.error.name);
+
+    reader.readAsText(file);
+})
